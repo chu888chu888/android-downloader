96bb9ce3cf04ef47be7a59af8393cc2e3a415e9f
snowdream <yanghui1986527@gmail.com>
2013-09-22 23:58:13 +0800

    [Add] library,sample

---
+++ sample/android-downloader-it/src/main/java/com/github/snowdream/android/net/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.github.snowdream.android.net.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.github.snowdream.android.net.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
